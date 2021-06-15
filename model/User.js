8f267e628d8f64421c38774609f3290139542588
Zhanibek Khojabekov <zkhodzhabekov@gmail.com>
2021-06-15 18:49:28 +0600

    first commit

---
+++ model/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: Number,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    subscribed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// module.exports = mongoose.model('Users', userSchema);
+module.exports = mongoose.model("User", userSchema);
