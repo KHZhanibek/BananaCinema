c8e8bf67f602db954f6bd612143cdb55da7b7497
Zhanibek Khojabekov <zkhodzhabekov@gmail.com>
2021-06-24 11:50:42 +0600

    make it better

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const {checkUser} = require('./checkuser');
 const { MovieDb } = require('moviedb-promise');
 const moviedb = new MovieDb('c7ac9277318b8c0f0f437c84fbc33bec');
+const totp = require("totp-generator");
 //console.log(checker(req, res));
 
 app.use(bodyParser.json());
